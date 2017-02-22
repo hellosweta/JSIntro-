abe17256cca9a5bed0c65f6a4204e390983987e1
swetagsanghavi <swetagsanghavi@gmail.com>
2017-02-22 12:16:14 -0800

    Starting intro js work W5D3

---
+++ IntroJS.js
@@ -0,0 +1,27 @@
+const uniq = function(array) {
+  let arr = []
+
+  array.forEach(function(el) {
+    if (arr.includes(el)) {
+    } else {
+      arr.push(el)};
+    }
+  );
+  return console.log(arr);
+};
+
+uniq([1, 2, 3, 2])
+
+// const twoSum
+//
+// const transpose
+//
+// const myEach
+//
+// const myMap
+//
+// const myInject
+//
+// const bubbleSort
+//
+// const substrings
